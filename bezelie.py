f58acacb11bdd521daf3048cc8666e19feaa6039
bezelie <nuja@live.jp>
2016-12-17 09:02:29 +0900

    Dec17

--- bezelie.py
+++ bezelie.py
@@ -12,17 +12,7 @@ address_pca9685 = 0x40 # If you connect other I2C devices, you might change this
 dutyMax = 490     #
 dutyMin = 110     #
 dutyCenter = 300  #
-headAdj = 0       # Head servo adjustment
-backAdj = 0       # Back servo adjustment
-stageAdj = 0      # Stage servo adjustment
-headMax = 360     # Downward limit
-headMin = 280     # Upward limit
-backMax = 380     # AntiClockwise limit
-backMin = 220     # Clockwise limit
-stageMax = 390    # AntiClockWise limit
-stageMin = 210    # Clocwise limit
 steps = 1         #
-defaultSpeed = 0  #
 
 # Global Valiables
 headNow = backNow = stageNow = dutyCenter
@@ -51,52 +41,53 @@ def setPCA9685Duty(channel, on, off):
   except IOError:
     pass
 
-def moveHead (degree, speed=defaultSpeed):
+def moveServo (id, degree, adj, max, min, speed, now):
+  dst = (dutyMin-dutyMax)*(degree+adj+90)/180 + dutyMax
+  if speed == 0:
+    setPCA9685Duty(id, 0, dst)
+    sleep(0.001 * math.fabs(dst-now))
+    now = dst    
+  if dst > max: dst = max
+  if dst < min: dst = min
+  while (now != dst):
+    if now < dst:
+      now += steps
+      if now > dst: now = dst
+    else:
+      now -= steps
+      if now < dst: now = dst
+    setPCA9685Duty(id, 0, now)
+    sleep(0.004 * steps *(speed))
+  return (now)
+
+def moveHead (degree, speed=1):
+  adj = 0       # Head servo adjustment
+  max = 360     # Downward limit
+  min = 280     # Upward limit
   global headNow
-  headDst = (dutyMin-dutyMax)*(degree+headAdj+90)/180 + dutyMax
-  if headDst > headMax: headDst = headMax
-  if headDst < headMin: headDst = headMin
-  while (headNow != headDst):
-    if headNow < headDst:
-      headNow += steps
-      if headNow > headDst: headNow = headDst
-    else:
-      headNow -= steps
-      if headNow < headDst: headNow = headDst
-    setPCA9685Duty(2, 0, headNow)
-    sleep(0.004 * steps *(speed + 1))
+  headNow = moveServo (2, degree, adj, max, min, speed, headNow)
 
-def moveBack (degree, speed=defaultSpeed):
+def moveBack (degree, speed=1):
+  adj = 0       # Back servo adjustment
+  max = 380     # AntiClockwise limit
+  min = 220     # Clockwise limit
   global backNow
-  backDst = (dutyMin-dutyMax)*(degree+backAdj+90)/180 + dutyMax
-  if backDst > backMax: backDst = backMax
-  if backDst < backMin: backDst = backMin
-  while (backNow != backDst):
-    if backNow < backDst:
-      backNow += steps
-      if backNow > backDst: backNow = backDst
-    else:
-      backNow -= steps
-      if backNow < backDst: backNow = backDst
-    setPCA9685Duty(1, 0, backNow)
-    sleep(0.004 * steps *(speed + 1))
+  backNow = moveServo (1, degree, adj, max, min, speed, backNow)
 
-def moveStage (degree, speed=defaultSpeed):
+def moveStage (degree, speed=1):
+  adj = 0      # Stage servo adjustment
+  max = 390    # AntiClockWise limit
+  min = 210    # Clocwise limit
   global stageNow
-  stageDst = (dutyMin-dutyMax)*(degree+stageAdj+90)/180 + dutyMax
-  if stageDst > stageMax: stageDst = stageMax
-  if stageDst < stageMin: stageDst = stageMin
-  while (stageNow != stageDst):
-    if stageNow < stageDst:
-      stageNow += steps
-      if stageNow > stageDst: stageNow = stageDst
-    else:
-      stageNow -= steps
-      if stageNow < stageDst: stageNow = stageDst
-    setPCA9685Duty(0, 0, stageNow)
-    sleep(0.004 * steps *(speed + 1))
+  stageNow = moveServo (0, degree, adj, max, min, speed,stageNow)
 
 def moveCenter ():
+    moveHead (0)
+    moveBack (0)
+    moveStage (0)
+
+# centering
+try:
     moveHead (20)
     moveHead (-20)
     moveHead (0)
@@ -106,6 +97,8 @@ def moveCenter ():
     moveStage (20)
     moveStage (-20)
     moveStage (0)
+except KeyboardInterrupt:
+  print " Interrupted by Keyboard"
 
 # ------------------Bezelie Pi Kit Legacy Module---------------------------
 from Adafruit_PWM_Servo_Driver import PWM
