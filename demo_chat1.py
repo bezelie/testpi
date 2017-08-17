fba67b30bb4d186363d33ecf61f49d3feedbb63d
bezelie <nuja@live.jp>
2017-08-18 00:14:51 +0900

    Aug15

--- demo_chat1.py
+++ demo_chat1.py
@@ -1,61 +1,51 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Bezelie demo Code for Raspberry Pi : Simple Conversation
 
-import time
-from time import sleep
-from datetime import datetime
-import datetime
-import csv
-from random import randint
-import subprocess
-import socket #  ソケット通信モジュール
+from datetime import datetime      # 現在時刻取得
+from random import randint         # 乱数の発生
+from time import sleep             # ウェイト処理
 import xml.etree.ElementTree as ET # XMLエレメンタルツリー変換モジュール
-import json
-import bezelie
+import subprocess                  #
+import threading                   # マルチスレッド処理
+import bezelie                     # べゼリー専用モジュール
+import socket                      # ソケット通信モジュール
+import json                        #
+import csv                         #
 
-csvFile = "chatDialog.csv"  # 対話リスト
+csvFile = "chatDialog.csv"   # 対話リスト
 jsonFile = "data_chat.json"  # 設定ファイル
-sensitivity = 30 
+sensitivity = 50             # マイク感度の設定。62が最大値。
 
 # Variables
-muteTime = 1  # 音声入力を無視する時間
-bufferSize = 256 # 受信するデータの最大バイト数。できるだけ小さな２の倍数が望ましい。
-alarmStop = False # アラーム
-beforeTime = time.time()
+muteTime = 1      # 音声入力を無視する時間
+bufferSize = 256  # 受信するデータの最大バイト数。できるだけ小さな２の倍数が望ましい。
+alarmStop = False # アラームのスヌーズ機能（非搭載）
 
-# 設定ファイルの読み込み
-f = open (jsonFile,'r')
-jDict = json.load(f)
-alarmOn = jDict['data1'][0]['alarmOn']
-alarmTime = jDict['data1'][0]['alarmTime']
-alarmKind = jDict['data1'][0]['alarmKind']
-alarmVol = jDict['data1'][0]['alarmVol']
-awake1Start = jDict['data1'][0]['awake1Start']
-awake1End = jDict['data1'][0]['awake1End']
-awake2Start = jDict['data1'][0]['awake2Start']
-awake2End  = jDict['data1'][0]['awake2End']
-print (jDict)
+# Servo Setting
+bez = bezelie.Control()               # べゼリー操作インスタンスの生成
+bez.setTrim(head=0, back=-5, stage=0) # センター位置の微調整
+bez.moveCenters()                     # ０番から７番までのサーボをセンタリング
+sleep(0.5)
 
+# Julius
 # Juliusをサーバモジュールモードで起動＝音声認識サーバーにする
 subprocess.call('sh openJTalk.sh "ちょっとまってて"', shell=True)
 print "Please Wait For A While"  # サーバーが起動するまで時間がかかるので待つ
 p = subprocess.Popen(["sh chat.sh"], stdout=subprocess.PIPE, shell=True)
-  # subprocess.PIPEは標準ストリームに対するパイプを開くことを指定するための特別な値
+# subprocess.PIPEは標準ストリームに対するパイプを開くことを指定するための特別な値
 pid = p.stdout.read()  # 終了時にJuliusのプロセスをkillするためプロセスIDをとっておく 
 print "Julius's Process ID =" +pid
-
 # TCPクライアントを作成しJuliusサーバーに接続する
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # clientオブジェクト生成
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # client.connect(('10.0.0.1', 10500))  # Juliusサーバーに接続
 client.connect(('localhost', 10500))  # Juliusサーバーに接続
 
 # Functions
-def writeFile(text):
+def writeFile(text): # デバッグ用機能
   f = open ('out.txt', 'r')
   textBefore = ""
   for row in f:
-#    print row+"\n"
+    print row+"\n"
     textBefore = textBefore + row
   f.close()
   f = open ('out.txt', 'w')
@@ -63,7 +53,7 @@ def writeFile(text):
   f.close()
   sleep(0.1)
 
-def replyMessage(keyWord):
+def replyMessage(keyWord):        # 対話
   # writeFile("reply start")
   data = []
   with open(csvFile, 'rb') as f:  # opening the datafile to read as utf_8
@@ -88,23 +78,20 @@ def replyMessage(keyWord):
       maxNum = i[2]               # Max probability
       ansNum = i[3]               # Index of answer
 
-  # Talk
-  subprocess.call('sudo amixer -q sset Mic 0', shell=True)  #
-  bezelie.actTalk()
-  # bezelie.moveHead (20)
+  # 発話
+  subprocess.call('sudo amixer -q sset Mic 0', shell=True)  # 自分の声を認識してしまわないようにマイクを切る
   print "Bezelie..."+data[ansNum][1]
 
-  if timeCheck():
+  if timeCheck(): # 活動時間かどうかをチェック
+    bez.moveRnd()
     subprocess.call('sh openJTalk.sh "'+data[ansNum][1]+'"', shell=True)
-  # subprocess.call('/home/pi/aquestalkpi/AquesTalkPi -s 120 "'+ data[ansNum ][1] +'" | aplay -q', shell=True)
+    bez.stop()
   else:
-    print "活動時間外なので発声はしません"
+    print "活動時間外なので発声・動作しません"
 
-#  alarmStop = True # アラームを止める
-  bezelie.moveCenter()
-  # bezelie.moveHead (0, 1)
+  alarmStop = True # アラームを止める
   sleep (muteTime)
-  subprocess.call('sudo amixer -q sset Mic 40', shell=True)  #
+  subprocess.call('sudo amixer -q sset Mic '+str(sensitivity), shell=True)  #
 
 def timeCheck(): # 活動時間内かどうかのチェック
   f = open (jsonFile,'r')
@@ -113,7 +100,7 @@ def timeCheck(): # 活動時間内かどうかのチェック
   awake1End = jDict['data1'][0]['awake1End']
   awake2Start = jDict['data1'][0]['awake2Start']
   awake2End  = jDict['data1'][0]['awake2End']
-  t = datetime.datetime.now()
+  t = datetime.now()
   if   int(t.hour) >  int(awake1Start[0:2]) and int(t.hour) <    int(awake1End[0:2]):
     flag = True
   elif int(t.hour) == int(awake1Start[0:2]) and int(t.minute) >= int(awake1Start[3:5]):
@@ -130,86 +117,86 @@ def timeCheck(): # 活動時間内かどうかのチェック
     flag = False # It is not Active Time
   return flag
 
-def alarmCheck():
-  global beforeTime
+def alarm():
   global alarmStop
-  flag = False
-  nowTime = time.time()
-  if (nowTime - beforeTime) > 10: # check by 20 seconds
-    f = open (jsonFile,'r')
-    jDict = json.load(f)
-    alarmOn = jDict['data1'][0]['alarmOn']
-    alarmTime = jDict['data1'][0]['alarmTime']
-    alarmKind = jDict['data1'][0]['alarmKind']
-    alarmVol = jDict['data1'][0]['alarmVol']
-    t = datetime.datetime.now()
-    print t.hour
-    print t.minute
-    print alarmTime[0:2]
-    print alarmTime[3:5]
+  f = open (jsonFile,'r')
+  jDict = json.load(f)
+  alarmOn = jDict['data1'][0]['alarmOn']
+  alarmTime = jDict['data1'][0]['alarmTime']
+  alarmKind = jDict['data1'][0]['alarmKind']
+  # alarmVol = jDict['data1'][0]['alarmVol']
+  now = datetime.now()
+  print 'Time: '+str(now.hour)+':'+str(now.minute)
 
-    if int(t.hour) == int(alarmTime[0:2]) and int(t.minute) == int(alarmTime[3:5]):
+  #if True: # アラーム動作のチェック用
+  if int(now.hour) == int(alarmTime[0:2]) and int(now.minute) == int(alarmTime[3:5]):
+    if alarmOn == "true":
       if alarmStop == False:
         print 'アラームの時間です'
-        flag = True
+        subprocess.call('sudo amixer -q sset Mic 0', shell=True)  #
+        if alarmKind == 'mild':
+          bez.moveAct('happy')
+          subprocess.call('sh openJTalk.sh "朝ですよ。"', shell=True)
+          bez.stop()
+        else:
+          bez.moveAct('happy')
+          subprocess.call('sh openJTalk.sh "朝だよー、起きてー、起きてー、起きてー"', shell=True)
+          bez.stop()
+        sleep (muteTime)
+        subprocess.call('sudo amixer -q sset Mic '+str(sensitivity), shell=True)  #
+      else:
+        print '_'
+        alarmStop = False
     else:
-      print '_'
-      alarmStop = False
-    beforeTime = nowTime
-  return flag
+      print 'アラームの時間ですが、アラームはオフになっています'
+  t=threading.Timer(20,alarm) # ｎ秒後にまたスレッドを起動する
+  t.setDaemon(True)           # メインスレッドが終了したら終了させる
+  t.start()
 
-def alarmRing():
-  writFile("Ring")
+# Set up
+subprocess.call('amixer cset numid=1 100%', shell=True)                  # Speakerr
+subprocess.call('sudo amixer -q sset Mic '+str(sensitivity), shell=True) # Mic
 
-# Get Started
-# bezelie.moveCenter()
-subprocess.call('sudo amixer -q sset Mic 40', shell=True)  # マイク感度の設定。62が最大値。
+t=threading.Timer(10,alarm)
+t.setDaemon(True)
+t.start()
 
 # Main Loop
-try:
-  data = ""
-  print "Please Speak"
-#  bezelie.actHappy()
-  subprocess.call('sh openJTalk.sh "もしもし"', shell=True)
-#  bezelie.moveCenter()
-#  writeFile("start")
-#  print timeCheck()
-  while True:
-#    flag = timeCheck()
-    if True:
+def main():
+  try:
+    data = ""
+    print "Please Speak"
+    bez.moveAct('happy')
+    subprocess.call('sh openJTalk.sh "どうも"', shell=True)
+    bez.stop()
+    while True:
       if "</RECOGOUT>\n." in data:  # RECOGOUTツリーの最終行を見つけたら以下の処理を行う
         try:
           # dataから必要部分だけ抽出し、かつエラーの原因になる文字列を削除する。
           data = data[data.find("<RECOGOUT>"):].replace("\n.", "").replace("</s>","").replace("<s>","")
-#           writeFile("data setted----------------------")
-#           writeFile(data)
           # fromstringはXML文字列からコンテナオブジェクトであるElement型に直接 $
           root = ET.fromstring('<?xml version="1.0"?>\n' + data)
           # root = ET.fromstring('<?xml version="1.0"?>\n' + data[data.find("<RECOGOUT>"):].replace("\n.", ""))
-#        writeFile("root setted----------------------")
-#        writeFile(root)
           keyWord = ""
           for whypo in root.findall("./SHYPO/WHYPO"):
             keyWord = keyWord + whypo.get("WORD")
             # writeFile("."+keyWord)
           print "You......."+keyWord
-#          writeFile("lets start reply")
           replyMessage(keyWord)
-#          writeFile("answerd")
         except:
           print "------------------------"
-#          writeFile("except")
         data = ""  # 認識終了したのでデータをリセットする
       else:
         data = data + client.recv(bufferSize)  # Juliusサーバーから受信
-#        writeFile("data added")
+        # /RECOGOUTに達するまで受信データを追加していく
 
-          # /RECOGOUTに達するまで受信データを追加していく
+  except KeyboardInterrupt: # CTRL+Cで終了
+    print "  終了しました"
+    p.kill()
+    subprocess.call(["kill " + pid], shell=True) # juliusのプロセスを終了
+    client.close()
+    bez.stop()
 
-except KeyboardInterrupt: # CTRL+Cで終了
-  print "  終了しました"
-#  writeFile("---------------------------")
-  p.kill()
-  subprocess.call(["kill " + pid], shell=True) # juliusのプロセスを終了
-  client.close()
-
+if __name__ == "__main__":
+#    pass
+    main()
